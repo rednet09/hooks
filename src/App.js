103ffb60fdec80a8419254949158e00882567196
Dhiraj Naik <naikdhiraj9@gmail.com>
2023-04-12 20:37:45 +0530

    updated changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(10);
+  const [theme, setTheme] = useState("blue");
+  function decrement() {
+    setCount((prevCount) => prevCount - 1);
+  }
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+    setTheme("red");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+
+      <button onClick={increment}>+</button>
+    </>
   );
 }
 
